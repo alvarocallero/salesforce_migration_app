d0ed5d14c5c13ee24684d84a1133fd2d27509d48
Abraham Kazerian <akazerian@altimetrik.com>
2017-07-27 10:36:36 -0300

    #48 - [JSButton][Wiki] Popup with or without redirect URL

---
+++ JS_Button/src/aura/JS_Button_PopUpRedirect/JS_Button_PopUpRedirectController.js
@@ -0,0 +1,52 @@
+({
+    handleReady: function(component, event, helper){
+        if (component.get('v.ready')){			
+            helper.execute(component, event, helper);
+            component.set('v.initialized', true);
+        }
+    },
+    doInit : function(component, event, helper) {
+        if (component.get('v.needsData') && !$A.util.isUndefinedOrNull(component.get('v.recordError'))){
+            //recordData doesnt work with the current object, so we need to get the data from a controller
+            var action = component.get("c.getObject");
+            action.setParams({
+                recordId: component.get('v.recordId'),
+                objectName: component.get('v.ObjectName'),
+                fieldList: component.get('v.fieldList')
+            });
+            action.setCallback(this,function(response){
+                if (response.getState() === "SUCCESS"){
+                    component.set('v.loadedRecord',  response.getReturnValue());
+                    component.set('v.ready', true);
+                }
+            });
+            $A.enqueueAction(action);
+        }else{
+            component.set('v.loadedRecord',  component.get('v.simpleRecord'));
+            component.set('v.ready', true);
+        }
+        
+    },
+    doneRendering: function(component, event, helper){
+        if (component.get('v.initialized') === true){
+            
+            //component.getEvent('closeActionEvent').fire();
+        }
+    },
+    //Crear el handlecancel hace el fire del close
+    handleCancel : function(component, event, helper){
+        var eventclose = $A.get("e.force:closeQuickAction");
+        eventclose.fire();
+    },
+    handleOk : function(component,event,helper){
+        var url = component.get("v.urlToRedirect");
+        var eventRedirect = $A.get("e.force:navigateToURL");
+        console.log("this is my url :", url);
+        eventRedirect.setParams({
+            "url": url
+        });
+        eventRedirect.fire();
+    }
+    //Crear el handleOk que hace el redireccionamiento
+    //	tomar la url del componente 
+})
