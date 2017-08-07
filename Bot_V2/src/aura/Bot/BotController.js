66d5e3d06ff29382569763ec1b635bf3e1586a33
Paulo Acosta <pacosta@altimetrik.com>
2017-08-07 16:59:16 -0300

    #76 - [Bot v2] - Add to the bot the notifications by chat and by toast

--- Bot_V2/src/aura/Bot/BotController.js
+++ Bot_V2/src/aura/Bot/BotController.js
@@ -1,4 +1,12 @@
 ({
+    
+    onCometdLoaded : function(component, event, helper) {
+  		var cometd = new org.cometd.CometD();
+  		component.set('v.cometd', cometd);
+  		if (component.get('v.sessionId') != null)
+    		helper.connectCometd(component);
+		},
+
     doInit : function(component, event, helper){
         var action = component.get("c.userInfo");
         action.setCallback(this, function(response){
@@ -14,9 +22,28 @@
 
             }
         });
+        
         $A.enqueueAction(action);
+        
+        component.set('v.cometdSubscriptions', []);
+  		
+  		// Disconnect CometD when leaving page
+  		window.addEventListener('unload', function(event) {
+    		helper.disconnectCometd(component);
+  		});
 
-
+  		// Retrieve session id
+  		var action2 = component.get('c.getSessionId');
+  		action2.setCallback(this, function(response) {
+    		if (component.isValid() && response.getState() === 'SUCCESS') {
+      			component.set('v.sessionId', response.getReturnValue());
+      			if (component.get('v.cometd') != null)
+        			helper.connectCometd(component);
+    		}
+    		else
+      			console.error(response);
+  		});
+  		$A.enqueueAction(action2);
 
     },
 
