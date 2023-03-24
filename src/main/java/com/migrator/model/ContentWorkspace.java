4c5195d887cfae3f6712495e68e2bba72e6bf237
alvarocallero <alvarocallero@gmail.com>
2023-03-24 10:51:54 -0300

    Update pom dependencies

--- src/main/java/com/migrator/model/ContentWorkspace.java
+++ src/main/java/com/migrator/model/ContentWorkspace.java
@@ -1,7 +1,7 @@
 package com.migrator.model;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class ContentWorkspace {
